a755f5fe028719ebc19037721a09bd5dcbaa4f77
alephpt <alephpt@github.com>
2024-09-23 22:00:58 -0500

    another failed attempt

--- client/src/sandbox/world/chunker.js
+++ client/src/sandbox/world/chunker.js
@@ -45,48 +45,50 @@ export default class Chunker {
             let z = 0;
             let chunk_position = { x: 0, z: 0 };
             let chunk_size = { h: 0, w: 0 };
+            let cPos_z = this.camera_position.z;
+            let cPos_x = this.camera_position.x;
 
-            console.log("Chunk Count", config.CHUNK_COUNT);
-            console.log("Chunk Size", config.CHUNK_SIZE);
-            console.log("Max Chunk Offset", config.MAX_CHUNK_OFFSET);
-            console.log("Max Offset Size", config.MAX_OFFSET_SIZE);
-            console.log("Mid Chunk Offset", config.MID_CHUNK_OFFSET);
-            console.log("Mid Offset Size", config.MID_OFFSET_SIZE);
-            console.log("Min Chunk Offset", config.MIN_CHUNK_OFFSET);
-            console.log("Min Offset Size", config.MIN_OFFSET_SIZE);
-            console.log("Max Chunk Count", config.MAX_CHUNK_COUNT);
-            console.log("Max Chunk Size", config.MAX_CHUNK_SIZE);
-            console.log("Mid Chunk Count", config.MID_CHUNK_COUNT);
-            console.log("Mid Chunk Size", config.MID_CHUNK_SIZE);
-            console.log("Min Chunk Count", config.MIN_CHUNK_COUNT);
-            console.log("Min Chunk Size", config.MIN_CHUNK_SIZE);
-            
+
+            // console.log("Chunk Count", config.CHUNK_COUNT);
+            // console.log("Chunk Size", config.CHUNK_SIZE);
+            // console.log("Max Chunk Offset", config.MAX_CHUNK_OFFSET);
+            // console.log("Max Offset Size", config.MAX_OFFSET_SIZE);
+            // console.log("Mid Chunk Offset", config.MID_CHUNK_OFFSET);
+            // console.log("Mid Offset Size", config.MID_OFFSET_SIZE);
+            // console.log("Min Chunk Offset", config.MIN_CHUNK_OFFSET);
+            // console.log("Min Offset Size", config.MIN_OFFSET_SIZE);
+            // console.log("Max Chunk Count", config.MAX_CHUNK_COUNT);
+            // console.log("Max Chunk Size", config.MAX_CHUNK_SIZE);
+            // console.log("Mid Chunk Count", config.MID_CHUNK_COUNT);
+            // console.log("Mid Chunk Size", config.MID_CHUNK_SIZE);
+            // console.log("Min Chunk Count", config.MIN_CHUNK_COUNT);
+            // console.log("Min Chunk Size", config.MIN_CHUNK_SIZE);
+            // console.log("Camera Position", cPos_z, cPos_x);
+
+            const distanceTo = (p1, p2) => 
+                { return Math.abs(p1 - p2); }
+
             while (z < config.CHUNK_COUNT)
                 {
-                    console.log("Z", z);
+                    // console.log("Z", z);
                     x = 0;
-
-                    // Calculate the Z position and size of the chunk 
+                    console.log("Z", z);
+                    let dx_z = distanceTo(z, cPos_z);
+                    console.log("Distance to Z", dx_z);
+                    
+                    // Calculate the Z position and size of the chunk for the max chunk size
                     if (z < config.MAX_CHUNK_OFFSET )
                         {
                             let z_pos = ((z + config.MAX_CHUNK_OFFSET) / 2) * config.CHUNK_SIZE;
                             chunk_position.z = -config.MAP_CENTER + z_pos;
                             chunk_size.h = config.MAX_OFFSET_SIZE;
                             z += config.MAX_CHUNK_OFFSET;
-                            console.log(" if z < MAX_CHUNK_OFFSET");
-                            console.log("Chunk Position", chunk_position);
-                            console.log("Chunk Size", chunk_size);
-                            console.log("Z", z);
                         }
                     else if (z >= (config.CHUNK_COUNT - config.MAX_CHUNK_OFFSET))
                         {
                             chunk_position.z = z * config.CHUNK_SIZE / 2;
                             chunk_size.h = config.MAX_OFFSET_SIZE;
                             z += config.MAX_CHUNK_OFFSET;
-                            console.log(" else if z >= CHUNK_COUNT - MAX_CHUNK_OFFSET");
-                            console.log("Chunk Position", chunk_position);
-                            console.log("Chunk Size", chunk_size);
-                            console.log("Z", z);
                         }
                     else
                         {
@@ -94,35 +96,47 @@ export default class Chunker {
                             chunk_position.z = z_pos - config.MAP_CENTER + config.MAX_CHUNK_OFFSET  * config.CHUNK_SIZE;
                             chunk_size.h = config.MAX_CHUNK_SIZE;
                             z += config.MAX_CHUNK_COUNT;
-                            console.log(" else");
-                            console.log("Chunk Position", chunk_position);
-                            console.log("Chunk Size", chunk_size);
-                            console.log("Z", z);
                         }
 
                     // Calculate the X position and size of the chunk
                     while (x < config.CHUNK_COUNT)
                         {
-                            if (x < config.MAX_CHUNK_OFFSET)
+                            console.log("X", x);
+                            let dx_x = distanceTo(x, cPos_x);
+                            console.log("Distance to X", dx_x);
+
+                            if (dx_z < (config.MID_CHUNK_COUNT / 2) && dx_x < (config.MID_CHUNK_COUNT / 2))
+                                {
+                                    console.log("Mid Chunk Size");
+                                    let z_pos = (z * config.CHUNK_SIZE) + config.MIN_CHUNK_SIZE / 2;
+                                    let x_pos = (x * config.CHUNK_SIZE) + config.MIN_CHUNK_SIZE / 2;
+                                    chunk_position = { x: x_pos - config.MAP_CENTER, z: z_pos - config.MAP_CENTER }
+                                    chunk_size = { w: config.MIN_CHUNK_SIZE, h: config.MIN_CHUNK_SIZE };
+                                    x += config.MIN_CHUNK_COUNT;
+                                }
+                            else if (dx_z < (config.MAX_CHUNK_COUNT / 2) && dx_x < (config.MAX_CHUNK_COUNT / 2)) 
+                                {
+                                    console.log("Max Chunk Size");
+                                    let z_pos = (z * config.CHUNK_SIZE) + config.MID_CHUNK_SIZE / 2;
+                                    let x_pos = (x * config.CHUNK_SIZE) + config.MID_CHUNK_SIZE / 2;
+                                    chunk_position = { x: x_pos - config.MAP_CENTER, z: z_pos - config.MAP_CENTER };
+                                    chunk_size = { h: config.MID_CHUNK_SIZE, w: config.MID_CHUNK_SIZE };
+                                    x += config.MID_CHUNK_COUNT;
+                                    console.log("Chunk Position", chunk_position);
+                                    console.log("Chunk Size", chunk_size);
+                                }
+                            else if (x < config.MAX_CHUNK_OFFSET)
                                 {
                                     let x_pos = ((x + config.MAX_CHUNK_OFFSET) / 2) * config.CHUNK_SIZE;
                                     chunk_position.x = -config.MAP_CENTER + x_pos;
                                     chunk_size.w = config.MAX_OFFSET_SIZE;
                                     x += config.MAX_CHUNK_OFFSET;
-                                    console.log(" if x < MAX_CHUNK_OFFSET");
-                                    console.log("Chunk Position", chunk_position);
-                                    console.log("Chunk Size", chunk_size);
-                                    console.log("X", x);
                                 }
                             else if (x >= config.CHUNK_COUNT - config.MAX_CHUNK_OFFSET)
                                 {
                                     chunk_position.x = x * config.CHUNK_SIZE / 2;
                                     chunk_size.w = config.MAX_OFFSET_SIZE;
                                     x += config.MAX_CHUNK_OFFSET;
-                                    console.log(" else if x >= CHUNK_COUNT - MAX_CHUNK_OFFSET");
-                                    console.log("Chunk Position", chunk_position);
-                                    console.log("Chunk Size", chunk_size);
-                                    console.log("X", x);
                                 }
                             else
                                 {
@@ -130,15 +144,16 @@ export default class Chunker {
                                     chunk_position.x = x_pos - config.MAP_CENTER + config.MAX_CHUNK_OFFSET * config.CHUNK_SIZE;
                                     chunk_size.w = config.MAX_CHUNK_SIZE;
                                     x += config.MAX_CHUNK_COUNT;
-                                    console.log(" else");
-                                    console.log("Chunk Position", chunk_position);
-                                    console.log("Chunk Size", chunk_size);
-                                    console.log("X", x);
                                 }
 
 
                             drawMesh(chunk_position, chunk_size);
                         }
+
+                    if (dx_z < (config.MID_CHUNK_COUNT / 2))
+                        { z += config.MIN_CHUNK_COUNT; }
+                    if (dx_z < (config.MAX_CHUNK_COUNT / 2))
+                        { z += config.MID_CHUNK_COUNT; }
                 }
         }
 
@@ -179,8 +194,8 @@ export default class Chunker {
 
     getCameraPosition = (x, z) => 
         {
-            let x_id = Math.floor(x / config.CHUNK_SIZE);
-            let z_id = Math.floor(z / config.CHUNK_SIZE);
+            let x_id = Math.floor(x / config.CHUNK_SIZE) + Math.floor(config.CHUNK_COUNT / 2);
+            let z_id = Math.floor(z / config.CHUNK_SIZE) + Math.floor(config.CHUNK_COUNT / 2);
             this.camera_position = { x: x_id, z: z_id };
         }
 
